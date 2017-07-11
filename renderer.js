a6628026253e3f3252b9ed92e18f77015c28a37f
Burak Bagdatli <burak@gatech.edu>
2017-07-11 00:03:01 -0400

    Initial change
    Sends a simple list to python, python adds numbers, sends data back, js
    shows it on the app.

--- renderer.js
+++ renderer.js
@@ -1,3 +1,17 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+const spawn = require('child_process').spawn;
+const scriptExecution = spawn("python.exe", ["simple_adder.py"]);
+
+// Handle normal output
+scriptExecution.stdout.on('data', (data) => {
+    console.log(String.fromCharCode.apply(null, data));
+    document.querySelector('.content .value').innerHTML = String.fromCharCode.apply(null, data);
+});
+
+// Write data (remember to send only strings or numbers, otherwhise python wont understand)
+var data = JSON.stringify([1,2,3,4,5,6]);
+scriptExecution.stdin.write(data);
+// End data write
+scriptExecution.stdin.end();
